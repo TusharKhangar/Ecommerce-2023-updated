baf1857267cc247b2194694033d27b19923f6fca
Tushar Khangar <tusharkhangar7490@gmail.com>
2023-04-17 22:27:00 +0530

    added checkout page and updated cart context along with it

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.contexts';
 
 import CustomButton from '../custom-button/custom-button.component';
@@ -8,17 +9,22 @@ import './cart-dropdown.style.scss';
 import React from 'react';
 
 
+
 const CartDropdown = () => {
 
     const {cartItems}=  useContext(CartContext);
+    const navigate = useNavigate();
 
+    const goToChekoutHandler = () => {
+        navigate('/checkout')
+    }
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map((item) =>  (
                 <CartItem key= {item.id} cartItem={item}/>))}
             </div>
-             <CustomButton>GO TO CHECKOUT</CustomButton>
+             <CustomButton onClick={goToChekoutHandler}>GO TO CHECKOUT</CustomButton>
         </div>
     )
 } 
